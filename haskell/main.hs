6cc709853ca07b7dec4bbfd9da4cd2f1e6416ea6
Edward Tanguay <edward@tanguay.info>
2025-04-16 01:57:28 +0200

    haskell ok

--- haskell/main.hs
+++ haskell/main.hs
@@ -1,2 +1,11 @@
+-- Main.hs
+module Main where
+
+import MathFunctions (add, multiply)
+
 main :: IO ()
-main = putStrLn "running Haskell"
+main = do
+    let sumResult = add 5 3
+    let productResult = multiply 5 3
+    putStrLn ("Sum: " ++ show sumResult)
+    putStrLn ("Product: " ++ show productResult)
