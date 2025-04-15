8837a7e4148f97d515f99c448de2e29e058cdffe
Edward Tanguay <edward@tanguay.info>
2025-04-16 01:58:33 +0200

    comments out

--- haskell/MathFunctions.hs
+++ haskell/MathFunctions.hs
@@ -1,10 +1,7 @@
--- MathFunctions.hs
 module MathFunctions (add, multiply) where
 
--- A simple function that adds two numbers
 add :: Int -> Int -> Int
 add x y = x + y
 
--- A function that multiplies two numbers
 multiply :: Int -> Int -> Int
 multiply x y = x * y
