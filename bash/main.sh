2cd495d10227bb8c9f5999b8f4fad2ee57aa83d3
Edward Tanguay (windows) <edward@tanguay.info>
2025-04-15 18:57:20 +0200

    bash in

---
+++ bash/main.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# Call the add function from math.sh
+source ./utils/math.sh
+result=$(add 5 3)
+echo "The result of addition is: $result"
---
+++ bash/utils/math.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+function add() {
+  echo $(($1 + $2))
+}
